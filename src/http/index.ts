5630b4a1206c7b94eb77635b3e7e26851e1d6177
gustavonikov <gustavonikov@gmail.com>
2020-11-08 03:35:28 -0300

    testing github actions

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const apiKey = 'a7543f02b7de71e3a39020604c7581f64c94c158c75a089d8c7d2be9ba4dfc54';
+
 export const cryptoHttp = axios.create({
     baseURL: 'https://min-api.cryptocompare.com/data',
     headers: {
-        authorization: `Apikey ${process.env.CRYPTO_COMPARE_KEY}`
+        authorization: `Apikey ${apiKey}`
     }
 });
